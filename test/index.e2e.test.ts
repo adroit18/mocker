250052522fb19ef53bdad9160b0f0379b5ab546a
Deepak Uniyal <deepakuniyal@microsoft.com>
2021-07-27 17:42:35 +0530

    Mocking logic

--- test/index.e2e.test.ts
+++ test/index.e2e.test.ts
@@ -29,104 +29,43 @@ test.describe("TextHighlightTextColor", async () => {
     // Create new document
     documentPage = await createNewDocument(initialPage);
 
-    documentPage.route("**/Box4Intl.js*", (route, request) => {
-      console.count("Box4Intl.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/Box4Intl.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/CommonIntl.js*", (route, request) => {
-      console.count("CommonIntl.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/CommonIntl.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/Compat.js*", (route, request) => {
-      console.count("Compat.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/Compat.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/EditSurface.css*", (route, request) => {
-      console.count("EditSurface.css, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/EditSurface.css",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/WordEditor.css*", (route, request) => {
-      console.count("WordEditor.css, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/WordEditor.css",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/jSanity.js*", (route, request) => {
-      console.count("jSanity.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/jSanity.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/MicrosoftAjaxDS.js*", (route, request) => {
-      console.count("MicrosoftAjaxDS.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/MicrosoftAjaxDS.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/WoncaIntl.js*", (route, request) => {
-      console.count("WoncaIntl.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/WoncaIntl.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/word.boot.js*", (route, request) => {
-      console.count("word.boot.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/word.boot.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/WordEditorIntl.js*", (route, request) => {
-      console.count("WordEditorIntl.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/WordEditorIntl.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
-    });
-    documentPage.route("**/require.js*", (route, request) => {
-      console.count("require.js, Fetched from mock");
-      route.fulfill({
-        path: "./mocks/require.js",
-        headers: {
-          "access-control-allow-origin": "*",
-        },
-      });
+    documentPage.route("**.js*", (route, request) => {
+      let path = "";
+      const url = request.url();
+      if (url.includes("require.js")) {
+        path = "./mocks/require.js";
+      } else if (url.includes("WordEditorIntl.js")) {
+        path = "./mocks/WordEditorIntl.js";
+      } else if (url.includes("word.boot.js")) {
+        path = "./mocks/word.boot.js";
+      } else if (url.includes("WoncaIntl.js")) {
+        path = "./mocks/WoncaIntl.js";
+      } else if (url.includes("MicrosoftAjaxDS.js")) {
+        path = "./mocks/MicrosoftAjaxDS.js";
+      } else if (url.includes("jSanity.js")) {
+        path = "./mocks/jSanity.js";
+      } else if (url.includes("WordEditor.css")) {
+        path = "./mocks/WordEditor.css";
+      } else if (url.includes("EditSurface.css")) {
+        path = "./mocks/EditSurface.css";
+      } else if (url.includes("Compat.js")) {
+        path = "./mocks/Compat.js";
+      } else if (url.includes("CommonIntl.js")) {
+        path = "./mocks/CommonIntl.js";
+      } else if (url.includes("Box4Intl.js")) {
+        path = "./mocks/Box4Intl.js";
+      } 
+      if(path){
+        console.count(`${path}, Fetched from mock`);
+        route.fulfill({
+          path,
+          headers: {
+            "access-control-allow-origin": "*",
+          },
+        });
+      } else{
+        route.continue();
+      }
     });
   });
 
