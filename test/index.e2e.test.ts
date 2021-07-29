7e591253b54e38df340b16dca07ca32684228c47
Deepak Uniyal <deepakuniyal@microsoft.com>
2021-07-29 12:23:25 +0530

    mocked separated requests

--- test/index.e2e.test.ts
+++ test/index.e2e.test.ts
@@ -2,7 +2,11 @@ import { chromium, ElementHandle } from "playwright";
 import { expect, test } from "@playwright/test";
 import { logIn } from "./utils/logIn";
 import { createNewDocument } from "./utils/createNewDocument";
-
+import fetch from "node-fetch";
+import https from "https";
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 let browser;
 let documentPage;
 let frame;
@@ -29,33 +33,55 @@ test.describe("TextHighlightTextColor", async () => {
     // Create new document
     documentPage = await createNewDocument(initialPage);
 
-    documentPage.route("**.js*", (route, request) => {
+    documentPage.route("**/*.js*", async (route, request) => {
       let path = "";
+      let content = "";
+      let contentType = "";
       const url = request.url();
-      if (url.includes("require.js")) {
-        path = "./mocks/require.js";
-      } else if (url.includes("WordEditorIntl.js")) {
-        path = "./mocks/WordEditorIntl.js";
-      } else if (url.includes("word.boot.js")) {
-        path = "./mocks/word.boot.js";
-      } else if (url.includes("WoncaIntl.js")) {
-        path = "./mocks/WoncaIntl.js";
-      } else if (url.includes("MicrosoftAjaxDS.js")) {
-        path = "./mocks/MicrosoftAjaxDS.js";
-      } else if (url.includes("jSanity.js")) {
-        path = "./mocks/jSanity.js";
-      } else if (url.includes("WordEditor.css")) {
+      if (url.includes("WordEditor.css")) {
         path = "./mocks/WordEditor.css";
       } else if (url.includes("EditSurface.css")) {
         path = "./mocks/EditSurface.css";
+      } else if (url.includes("require.js")) {
+        path = "./mocks/require.js";
+      } else if (url.includes("WordEditorIntl.js")) {
+        path = "./mocks/WordEditorIntl.js";
+      } else if (url.includes("WoncaIntl.js")) {
+        path = "./mocks/WoncaIntl.js";
+      } else if (url.includes("jSanity.js")) {
+        path = "./mocks/jSanity.js";
       } else if (url.includes("Compat.js")) {
         path = "./mocks/Compat.js";
       } else if (url.includes("CommonIntl.js")) {
         path = "./mocks/CommonIntl.js";
       } else if (url.includes("Box4Intl.js")) {
         path = "./mocks/Box4Intl.js";
-      } 
-      if(path){
+      } else if (url.includes("wacBoot")) {
+        // path = "./mocks/wacBoot.min.js";
+        var wacBootContent = await fetch(
+          "https://localhost:8080/dist/wacBoot.js",
+          { agent: httpsAgent }
+        );
+        content = await wacBootContent.text();
+        contentType = "application/javascript; charset=UTF-8";
+        // path = "C:/msft/1JS/ooui/packages/onenote-online-boot/lib/WacBoot.js";
+      } else if (url.includes("MicrosoftAjaxDS.js")) {
+        path =
+          "C:/msft/1JS/ooui/packages/wac-microsoftajax/src/MicrosoftAjaxDS.js";
+      } else if (url.includes("word.boot.js")) {
+        path =
+          "C:/msft/1JS/ooui/packages/office-online-jsapi/dist/word.boot.js";
+      } else if (url.includes("midgardbootstrapper.js")) {
+        path = "C:/msft/1JS/ooui/apps/word-online/dist/midgardbootstrapper.js";
+      } else if (url.includes("suiteux.shell.core.js")) {
+        path =
+          "C:/msft/1JS/ooui/apps/word-online/dist/suiteux-shell/js/suiteux.shell.core.js";
+      } else if (url.includes("suiteux.shell.plus.js")) {
+        path =
+          "C:/msft/1JS/ooui/apps/word-online/dist/suiteux-shell/js/suiteux.shell.plus.js";
+      }
+
+      if (path) {
         console.count(`${path}, Fetched from mock`);
         route.fulfill({
           path,
@@ -63,7 +89,81 @@ test.describe("TextHighlightTextColor", async () => {
             "access-control-allow-origin": "*",
           },
         });
-      } else{
+      } else if (content) {
+        console.count(`${path}, Fetched from mock`);
+        route.fulfill({
+          status: 200,
+          body: content,
+          headers: {
+            "access-control-allow-origin": "*",
+          },
+          contentType,
+        });
+      } else {
+        route.continue();
+      }
+    });
+    documentPage.route("**/RemoteUls.ashx*", (route, request) => {
+      console.log("RemoteUls calls mocked");
+      route.fulfill({ status: 200 });
+    });
+    documentPage.route("**/RemoteTelemetry.ashx*", (route, request) => {
+      console.log("RemoteTelemetry calls mocked");
+      route.fulfill({ status: 200 });
+    });
+    documentPage.route("**/*.woff*", (route, request) => {
+      let path = "";
+      const url = request.url();
+      if (url.includes("fabric-icons")) {
+        path = "./mocks/fabric-icons-0-467ee27f.woff";
+      } else if (url.includes("sharedheaderplaceholder-icons")) {
+        path = "./mocks/sharedheaderplaceholder-icons.woff";
+      }
+      if (path) {
+        console.count(`${path}, Fetched from mock`);
+        route.fulfill({
+          path,
+          headers: {
+            "access-control-allow-origin": "*",
+          },
+        });
+      } else {
+        route.continue();
+      }
+    });
+    documentPage.route("**/*.ico*", (route, request) => {
+      let path = "";
+      const url = request.url();
+      if (url.includes("FavIcon_Word.ico")) {
+        path = "./mocks/FavIcon_Word.ico";
+      }
+      if (path) {
+        console.count(`${path}, Fetched from mock`);
+        route.fulfill({
+          path,
+          headers: {
+            "access-control-allow-origin": "*",
+          },
+        });
+      } else {
+        route.continue();
+      }
+    });
+    documentPage.route("**/*.ashx*", (route, request) => {
+      let path = "";
+      const url = request.url();
+      if (url.includes("AppSettingsHandler.ashx")) {
+        path = "./mocks/AppSettingsHandler.ashx.json";
+      }
+      if (path) {
+        console.count(`${path}, Fetched from mock`);
+        route.fulfill({
+          path,
+          headers: {
+            "access-control-allow-origin": "*",
+          },
+        });
+      } else {
         route.continue();
       }
     });
